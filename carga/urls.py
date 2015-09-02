84619f85b6cf7690a39c4edddb99531c981f96b6
asullom <asullom@gmail.com>
2015-09-02 15:47:23 -0500

    NaturalPerson API

--- carga/urls.py
+++ carga/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework import routers
-from carga.views import UserViewSet, CicloViewSet
+from carga.views import UserViewSet, CicloViewSet, NaturalPersonViewSet
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 
 router.register(r'users', UserViewSet)
 router.register(r'ciclos', CicloViewSet)
+router.register(r'naturalpersons', NaturalPersonViewSet)
 
 urlpatterns = patterns(
     '',
--- carga/views.py
+++ carga/views.py
@@ -3,7 +3,20 @@ from django.conf.urls import url, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 from rest_framework import permissions
-from .models import Ciclo
+from .models import Ciclo, NaturalPerson
+
+
+class NaturalPersonSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = NaturalPerson
+        #fields = ('url', 'abrev', 'desc')
+
+
+class NaturalPersonViewSet(viewsets.ModelViewSet):  # API REST
+    queryset = NaturalPerson.objects.filter()
+    serializer_class = NaturalPersonSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class CicloSerializer(serializers.HyperlinkedModelSerializer):
